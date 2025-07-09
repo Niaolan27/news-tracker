b6ca39832a8630df1bd9c01418ff37b104fab825
Niaolan27 <jniow@andrew.cmu.edu>
2025-07-09 23:30:58 +0800

    updated ngrok again with build

--- news_tracker_flutter/build/web/flutter_service_worker.js
+++ news_tracker_flutter/build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "64398898fedcdfe919fbb322fc322956",
+const RESOURCES = {"flutter_bootstrap.js": "5fb7d067711c76bf910c0468342c33ed",
 "version.json": "f3d142bebcf374b8f08905b33c13b50b",
 "index.html": "49a176d77fca3f0269e53b3f78851eba",
 "/": "49a176d77fca3f0269e53b3f78851eba",
-"main.dart.js": "de193d909da6fd757105a6ff8d2aa6ac",
+"main.dart.js": "caf572e5356d98cf30f12a022e0ccc70",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
